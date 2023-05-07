7b9d3046f5a757881c0a3b432c7d0ea5d1285ef6
MehrCodeLand <mehrcodeland@gmail.com>
2023-05-08 00:46:27 +0330

    add SpeechSynthesizerLeyer

--- laraV1/Speeches/SpeechSynthesizerLeyer.cs
+++ laraV1/Speeches/SpeechSynthesizerLeyer.cs
@@ -10,5 +10,34 @@ namespace laraV1.Speeches
     public class SpeechSynthesizerLeyer
     {
         SpeechSynthesizer speek = new SpeechSynthesizer();
+
+        private int MainSpeekerVolum;
+        public int SpeekerVolume { 
+            get { return MainSpeekerVolum; } 
+            set {
+                
+                if(value >= 0 || value <= 100)
+                {
+                    MainSpeekerVolum = value;
+                    speek.Volume = value;
+                }
+
+                MainSpeekerVolum = 100;
+                speek.Volume = 100;
+            }
+        }
+        public void SetOutPut()
+        {
+            speek.SetOutputToDefaultAudioDevice();
+        }
+        public void SelectGenderVoice( bool IsMale)
+        {
+            if(IsMale == true)
+            {
+                speek.SelectVoice("Microsoft David Desktop");
+            }
+
+            speek.SelectVoice("Microsoft Zira Desktop");
+        }
     }
 }
